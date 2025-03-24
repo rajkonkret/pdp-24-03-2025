ec353a438902fec2295373f5c9e97d9710b072da
radoslawjaniak <rajkonkret660@gmail.com>
2025-03-24 15:48:47 +0100

    [1 16] update typy_danych_3_krotka.py

--- day_1/typy_danych_3_krotka.py
+++ day_1/typy_danych_3_krotka.py
@@ -29,3 +29,47 @@ print(tupla_liczby)  # NameError: name 'tupla_liczby' is not defined
 
 print(tupla_imiona.index("Radek"))  # indeks 0
 print(tupla_imiona.count("Radek"))  # występuje 1 raz
+
+# rozpakowanie tupli
+tup = 1, 2
+print(type(tup))  # <class 'tuple'>
+
+a, b = 1, 2
+
+a, b = tup  # 1, 2
+print(f"{a=}, {b=}")  # a=1, b=2
+
+tup2 = 1, 2, 3
+# a, b = tup2 # ValueError: too many values to unpack (expected 2)
+a, *b = tup2  # * worek na pozostałe dane
+print(f"{a=}, {b=}")  # a=1, b=[2, 3]
+
+print(tupla_imiona)  # ('Radek', 'Tomek', 'Zenek', 'Anna', 'Marek')
+# name1, name2, name3
+
+name1, name2, *name3 = tupla_imiona
+print(f"{name1=}, {name2=}, {name3=}")
+# name1='Radek', name2='Tomek', name3=['Zenek', 'Anna', 'Marek']
+
+*name1, name2, name3 = tupla_imiona
+print(f"{name1=}, {name2=}, {name3=}")
+# name1=['Radek', 'Tomek', 'Zenek'], name2='Anna', name3='Marek'
+
+name1, *name2, name3 = tupla_imiona
+print(f"{name1=}, {name2=}, {name3=}")
+# name1='Radek', name2=['Tomek', 'Zenek', 'Anna'], name3='Marek'
+
+# sortowanie krotki zwraca listę
+print(sorted(tupla_imiona))  # ['Anna', 'Marek', 'Radek', 'Tomek', 'Zenek']
+print(tupla_imiona)  # krotka nie ulega zmianie
+print(type(tupla_imiona))  # <class 'tuple'>
+
+lista_sorted = sorted(tupla_imiona)
+print(type(lista_sorted))  # <class 'list'>
+print(lista_sorted)  # ['Anna', 'Marek', 'Radek', 'Tomek', 'Zenek']
+
+lista = list(tupla_imiona)
+print(lista)
+print(type(lista))
+# ['Radek', 'Tomek', 'Zenek', 'Anna', 'Marek']
+# <class 'list'>
