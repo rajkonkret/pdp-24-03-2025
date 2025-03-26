b55ecef851402ec280b83e42c827b9bacb7a3e37
radoslawjaniak <rajkonkret660@gmail.com>
2025-03-26 14:27:31 +0100

    [3 18] add matematyka.py

---
+++ day_3/matematyka.py
@@ -0,0 +1,11 @@
+import math
+
+print(math.pi)  # 3.141592653589793
+
+print(math.sqrt(25))  # 5.0
+
+angle_degrees = 30
+angle_radians = math.radians(angle_degrees)
+sin_value = math.sin(angle_radians)
+
+print(sin_value)  # 0.49999999999999994
