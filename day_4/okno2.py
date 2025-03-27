1c2b38b50a0c74e3ad513ac19a2af15c26b5dea0
radoslawjaniak <rajkonkret660@gmail.com>
2025-03-27 15:27:56 +0100

    [4 21] add okno2.py

---
+++ day_4/okno2.py
@@ -0,0 +1,17 @@
+# Generated with tkedit (tkedit.glitch.me)
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+
+root = Tk()
+
+root.geometry('254x200')
+root.configure(background='#F0F8FF')
+root.title('Hello, I\'m the main window')
+
+# Create a label
+Label(root, text='this is a label', bg='#F0F8FF', font=('arial', 12, 'normal')).place(x=501, y=44)
+
+root.mainloop()
+
