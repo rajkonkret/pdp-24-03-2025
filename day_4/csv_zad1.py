1ffa401d62f438d5c4608adc8a6ada5198da1a4e
radoslawjaniak <rajkonkret660@gmail.com>
2025-03-27 12:39:37 +0100

    [4 7] update csv_zad1.py

--- day_4/csv_zad1.py
+++ day_4/csv_zad1.py
@@ -22,4 +22,22 @@ filename = "records_dict.csv"
 with open(filename, "w", newline="") as csv_fh:
     csvwriter = csv.DictWriter(csv_fh, fieldnames=fields)
     csvwriter.writeheader()
-    csvwriter.writerow(dict)
+    csvwriter.writerow(dict) # zapis słownika
+
+products = [
+    {"sku": 1, "exp_date": 'today', "price": 100},
+    {"sku": 2, "exp_date": 'today', "price": 200},
+    {"sku": 3, "exp_date": 'tomorrow', "price": 500},
+    {"sku": 4, "exp_date": 'today', "price": 50.00},
+    {"sku": 5, "exp_date": 'today', "price": 999},
+]
+
+filename = "records_products.csv"
+fields_product = [key for key in products[0]]
+print(fields_product)  # ['sku', 'exp_date', 'price']
+
+with open(filename, "w", newline="") as f:
+    csvwriter = csv.DictWriter(f, fieldnames=fields_product, delimiter=";")
+    csvwriter.writeheader()
+    csvwriter.writerows(products) # zapis listy
+
